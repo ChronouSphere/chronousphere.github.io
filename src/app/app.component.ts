b9320126b4f41d6225c1357ee7e5c046f716f159
Muhammad Ariff Bin Rosli <ariff@hitachi-ebworx.com>
2023-03-27 17:28:50 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'epf-locator';
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit() {
+    const body = {"ios":"100", "lan":"EN","ver":"100"};
+    this.http.post('https://secure.kwsp.gov.my/m2/postBranchLocation', body)
+    .subscribe((res) => {
+      console.log(res);
+    })
+  }
 }
